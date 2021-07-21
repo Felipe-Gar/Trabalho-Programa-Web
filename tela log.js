3abbcb6f559ad3596ce4a3890a02bdf62bc38555
Felipe Garcia <felipesgarcia10@gmail.com>
2021-07-21 13:16:39 -0300

    tem todos os codigos do trabalho

---
+++ tela log.js
@@ -0,0 +1,11 @@
+function Acessar(){
+    var username = document.getElementById("username").value;
+    var senha = document.getElementById("senha").value;
+
+    if(username =="" ,senha==""){
+        alert("Preencha Por favor");
+
+    }else{
+       var calcu = window.open("Escolha.html")
+    }
+}
